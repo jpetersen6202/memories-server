169a7569383ff1c99f2da38b61b056b15c940361
Jake P <jpetersen6202@gmail.com>
2022-01-19 16:36:28 -0600

    corrected app listen, basic starting endpoint

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
 const app = express();
+
+app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
@@ -12,4 +16,6 @@ app.use(cors())
 const CONNECTION_URL = 'mongodb+srv://jpetersen6202:data123@memories.cppuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Server listening on port ${PORT}`)})
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+
+mongoose.connect(CONNECTION_URL).then(()=>{console.log(`Database connected.`)})
