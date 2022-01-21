f2dd2f49c6d99eeef9de42a4cf89eefd13e83c7d
Jake P <jpetersen6202@gmail.com>
2022-01-21 11:54:31 -0600

    moved cors call above the definition of the post routes

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ import postRoutes from './routes/postsRouter.js'
 
 const app = express();
 
-app.use('/posts', postRoutes)
-
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://jpetersen6202:data123@memories.cppuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000
