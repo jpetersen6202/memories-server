320007d21f2247d60f2bab646bd22ae61f135574
Jake P <jpetersen6202@gmail.com>
2022-01-25 20:46:02 -0600

    added endpoint and controller logic for deleting a post

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -29,3 +29,11 @@ export const updatePost = async (req, res) => {
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, {new: true})
     res.json(updatedPost)
 }
+
+export const deletePost = async (req, res) => {
+    const {id: _id} = req.params
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with id given was found.')
+
+    await PostMessage.findByIdAndRemove(_id)
+    return res.json({message: 'Post deleted successfully'})
+}
